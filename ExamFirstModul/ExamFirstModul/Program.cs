238cdd6d98660ec0e3d1ce75e4ebcdf6218b1f42
SaidakbarX <khayumkhujayev6937@gmail.com>
2024-11-29 14:27:37 +0500

    update program
    update program

---
+++ ExamFirstModul/ExamFirstModul/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ExamFirstModul
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("hello Akobir");
+        }
+    }
+}

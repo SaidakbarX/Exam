333efdeb676e22bb7d0535fb640179aa8c6ec085
SaidakbarX <khayumkhujayev6937@gmail.com>
2024-11-30 11:07:18 +0500

    update

--- ExamFirstModul/ExamFirstModul/Program.cs
+++ ExamFirstModul/ExamFirstModul/Program.cs
@@ -1,12 +1,49 @@
-﻿namespace ExamFirstModul
+﻿using System.Xml.Linq;
+
+namespace ExamFirstModul
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("hello Akobir");
-            Console.WriteLine("hello Saidakbar");
+           
         }
+        //1-savol
+        public static int TwoDigitsNumbers(List<int> numbers)
+        {   var count = 0;
+            foreach (var number in numbers)
+            {
+                if (10 < number && number > 99) count++;
+            }
+            return count;
+        }
+        //2-savol
+        public static string RemoveLastTwo(string delete)
+        {
+         delete = delete.Substring(0,delete.Length-2);
+            return delete;
+        }
+        // 3-savol
+        public static bool AllElementsAreUppercase (List<string> elements)
+        {
+        for (var i = 0; i < elements.Count; i++)
+            {
+                if (i == char.ToLower())
+                {
+
+                }
+            }
+        }
+        //4-misol
+        public static bool StartEnd(string word)
+        {
+            if ((word.StartsWith== 'qovun')  &&  (word.EndsWith == 'qovun') )
+            {
+                return true;
+            }
+            return false;
+        }
+
+
     }
 }
---
+++ ExamFirstModul/ExamFirstModul/Service/PhoneService.cs
@@ -0,0 +1,61 @@
+﻿using ExamFirstModul.Models;
+
+namespace ExamFirstModul.Service;
+
+public class PhoneService
+{
+    private List<PhoneModel> _phone;
+    public PhoneService()
+    {
+        _phone = new List<PhoneModel>();
+    }
+    public bool AddPhone(PhoneModel phone)
+    {
+        try
+        {
+
+            phone.Id = Guid.NewGuid();
+            _phone.Add(phone);
+            return true;
+        }
+        catch
+        {
+
+            return false;
+        }
+    }
+    public PhoneModel GetByIdPhone(Guid id)
+    {
+        foreach (var phone in _phone)
+        {
+            if (phone.Id == id) return phone;
+        }
+        return null;
+    }
+    public bool UpdatePhone(Guid id)
+    {
+        var phoneId = GetByIdPhone(id);
+        if (phoneId is null)
+        {
+            return false;
+        }
+        var index = _phone.IndexOf(phoneId);
+        _phone[index] = phoneId;
+        return true;
+
+    }
+    public bool DeletePhone(Guid id)
+    {
+        var phoneDelete = GetByIdPhone(id);
+        if (phoneDelete is null)
+        {
+            return false;
+        }
+        _phone.Remove(phoneDelete);
+        return true;
+    }
+    public List<PhoneModel> GetAllPhones()
+    {
+        return _phone;
+    }
+}
